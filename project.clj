0fa9b4b835ba07164850aa3447f85b6fbaa3a44d
Roger Allen <rallen@gmail.com>
2013-01-09 22:08:29 -0800

    Adjust the project to reflect the branch & add unicode support. Add a couple things I wanted right off... - all the midi notes, not just one octave. - add   and   aliases for sharp & flat functions.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject leipzig "0.2.0"
+(defproject leipzig "0.2.0-RALLEN"
   :description "A composition library for Overtone."
   :url "http://github.com/ctford/leipzig"
   :license {:name "The MIT License"
@@ -9,4 +9,5 @@
     [org.clojure/clojure "1.4.0"]
     [overtone "0.7.1" :exclusions [seesaw]]
     [midje "1.4.0"]
-    [org.clojure/math.numeric-tower "0.0.1"]])
+    [org.clojure/math.numeric-tower "0.0.1"]]
+  :jvm-opts ["-Dfile.encoding=UTF-8"])
--- src/leipzig/scale.clj
+++ src/leipzig/scale.clj
@@ -32,10 +32,28 @@
 
 (defn- from [base] (partial + base))
 
-(defs [sharp flat] [inc dec])
+(defs [sharp ♯ flat ♭] [inc inc dec dec])
+
 (defs [C D E F G A B]
   (map
     (comp from (from 60) major)
+    (range)))
+
+;; all the midi pitches
+(defs [C-1 D-1 E-1 F-1 G-1 A-1 B-1
+       C0 D0 E0 F0 G0 A0 B0
+       C1 D1 E1 F1 G1 A1 B1
+       C2 D2 E2 F2 G2 A2 B2
+       C3 D3 E3 F3 G3 A3 B3
+       C4 D4 E4 F4 G4 A4 B4
+       C5 D5 E5 F5 G5 A5 B5
+       C6 D6 E6 F6 G6 A6 B6
+       C7 D7 E7 F7 G7 A7 B7
+       C8 D8 E8 F8 G8 A8 B8
+       C9 D9 E9 F9 G9 A9 B9 ;; note A9 & B9 are > 127
+       ]
+  (map
+    (comp from (from 0) major)
     (range)))
 
 (defn mode [scale n] (comp #(- % (scale n)) scale (from n)))
